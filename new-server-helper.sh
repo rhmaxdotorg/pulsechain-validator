bd198c6a3a61eac85ab3dd7ede346d5ffe45428c
rhmaxdotorg <100790377+rhmaxdotorg@users.noreply.github.com>
2023-04-30 13:02:33 -0700

    more accurate name and description

--- new-server-helper.sh
+++ new-server-helper.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #
-# PulseChain validator helper script for Amazon AWS EC2 to update to just do a few nice things
+# PulseChain validator helper script to use on initial server setup (just do a few nice things)
 #
 # Note: once complete, it will reboot and disconnect your SSH session temporarily, wait a minute and log back in for freshness
 #
