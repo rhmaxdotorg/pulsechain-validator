d0eba6212ae85c7178dbdb55b037b7ed5fa0024a
rhmaxdotorg <100790377+rhmaxdotorg@users.noreply.github.com>
2023-05-01 19:33:13 -0700

    no longer does it remove blockchain data (by default)

--- reset-validator.sh
+++ reset-validator.sh
@@ -3,6 +3,10 @@
 # PulseChain validator helper script stuff to "reset" things so you can try again
 #
 
+GETH_DIR="/opt/geth"
+LIGHTHOUSE_DIR="/opt/lighthouse"
+REMOVE_BLOCKCHAIN_DATA=false
+
 I_KNOW_WHAT_I_AM_DOING=false # CHANGE ME ONLY IF YOU TRULY UNDERSTAND
 
 if [ "$I_KNOW_WHAT_I_AM_DOING" = false ]; then
@@ -23,8 +27,14 @@ sudo rm -rf /etc/systemd/system/geth.service
 sudo rm -rf /etc/systemd/system/lighthouse*
 sudo systemctl daemon-reload
 
-# remove client data
-sudo rm -rf /opt/*
+# remove client data (leave blockchain by default)
+if [ "$REMOVE_BLOCKCHAIN_DATA" = false ]; then
+    sudo find $GETH_DIR -mindepth 1 -name data -prune -o -exec rm -rf {} +
+    sudo find $LIGHTHOUSE_DIR -mindepth 1 -name beacon -prune -o -exec rm -rf {} +
+else
+    sudo rm -rf $GETH_DIR
+    sudo rm -rf $LIGHTHOUSE_DIR
+fi
 
 # remove the user
 sudo userdel -r node &>/dev/null
