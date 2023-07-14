bd21d37a0ae366d09763cb83574ade8f15b35cfb
rhmaxdotorg <100790377+rhmaxdotorg@users.noreply.github.com>
2023-07-14 11:24:34 -0700

    added snap to apt packages for setup

--- pulsechain-validator-setup.sh
+++ pulsechain-validator-setup.sh
@@ -37,7 +37,7 @@
 
 # general config
 NODE_USER="node"
-APT_PACKAGES="build-essential cmake clang curl git wget jq protobuf-compiler"
+APT_PACKAGES="build-essential cmake clang curl git wget jq protobuf-compiler snap"
 
 shopt -s dotglob
 
